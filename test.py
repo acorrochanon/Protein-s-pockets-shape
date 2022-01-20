e2c1c5f1f633a3e6f08bb67322d81771a5994c60
Yossra Gharbi <yossra944@gmail.com>
2022-01-20 04:21:32 +0100

    Finish the new ML approach [issue 53]

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 print('hello')
 print('This is a message from my local computer')
 
-print("some work on brunch iss53")
+print("some work on branch iss53")
+print("finish the work on branch hotfix")
