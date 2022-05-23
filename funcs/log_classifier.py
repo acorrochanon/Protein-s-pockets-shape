6466af5ec9023de56415aec0b8eea2b1b63d8a19
Alejandro Corrochano <56264560+acorrochanon@users.noreply.github.com>
2022-05-23 10:43:13 +0200

    Geometric features
    Creation of new variables containing fpocket's geometric features.

--- funcs/log_classifier.py
+++ funcs/log_classifier.py
@@ -9,12 +9,13 @@ abs_path = '/'.join([p for p in current_folder.split('/')[:-1]])+'/'
 
 if __name__ == '__main__':
 
-    train_fp_features = torch.load(abs_path + 'data/Features/train_features.pt')
+    # Load train and test data. 
+    train_fp_features = torch.load(abs_path + 'data/Features/geo_train_features.pt')
     train_labels = torch.load(abs_path + 'data/Labels/train_labels.pt')
-    test_fp_features = torch.load(abs_path + 'data/Features/test_features.pt')
+    test_fp_features = torch.load(abs_path + 'data/Features/geo_test_features.pt')
     test_labels = torch.load(abs_path + 'data/Labels/test_labels.pt')
 
-    # PREPARE AND FILL DATA STRUCTURES
+    # Generate dictionaries where each key represents a feature. 
     train_dict, test_dict = {}, {}
     keys = range(len(train_fp_features[0]))
     for i in keys:
@@ -29,22 +30,17 @@ if __name__ == '__main__':
         for idx, feat_test in enumerate(pocket_test):
             test_dict[idx].append(float(feat_test))
 
-    # CREATE DF
+    # Create dataframes based on dictionaries and standardize them. 
     train_df = pd.DataFrame(data = train_dict)
     stand_train_df = (train_df - train_df.mean()) / train_df.std()
-
     test_df = pd.DataFrame(data = test_dict)
     stand_test_df = (test_df - test_df.mean()) / test_df.std()
 
-    # print(f'Train dataset shape:{train_df.shape}')
-    # print(f'Test dataset shape:{test_df.shape}')
-
-    # MODEL
     model = LogisticRegression().fit(stand_train_df, train_labels)
     y_pred = model.predict(stand_test_df)
     y_pred_proba = model.predict_proba(stand_test_df)[:,1]
 
-    # METRICS
+    # Metrics
     accuracy = model.score(stand_test_df, test_labels)
     auc_score = roc_auc_score(test_labels, y_pred_proba)
     print(f'Logistic regression -> Accuracy: {accuracy:.2f} | AUC: {auc_score:.3f}')
